bc65db68181c09ba275363b083a699f3e8e1bc93
Vamshi1811 <56248860+Vamshi1811@users.noreply.github.com>
2019-12-19 00:44:55 +0530

    Create helloworldtest.java

---
+++ helloworldtest.java
@@ -0,0 +1,22 @@
+package org.jacoco.examples.java.gradle;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class helloworldTest {
+
+	private helloworld subject;
+
+	@Before
+	public void setup() {
+		subject = new helloworld();
+	}
+	
+	@Test
+	public void testGetMessage() {
+		assertEquals("Hello, World!", subject.getMessage(false));
+	}
+	
+}
